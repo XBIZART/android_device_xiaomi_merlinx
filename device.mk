7ccefa07c7ab067f73cace4e5072b2ddbb309725
fourninesix <klmajor2213@gmail.com>
2021-08-24 07:12:16 +0000

    merlin: Use POSP apns config

--- device.mk
+++ device.mk
@@ -31,9 +31,14 @@ $(call inherit-product, vendor/mediatek/ims/mtk-ims.mk)
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/audio_policy_configuration.xml
 
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     libldacBT_dec
+
+# Apn
+PRODUCT_COPY_FILES += \
+   $(DEVICE_PATH)/configs/apns-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
 
 # CameraGo
 PRODUCT_PACKAGES += \
@@ -103,7 +108,4 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     WifiOverlay \
     TetheringConfigOverlay
-    
-# Vendor overlay
-PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/vendor-overlay/,$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION))
+
