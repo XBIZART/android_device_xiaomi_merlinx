77c954594e45a88a5c8ff70d415ca137dabf7deb
XBIZART <dwiyohan9@gmail.com>
2022-02-16 09:54:33 +0000

    merlinx: Use Raven FP February
    Signed-off-by: XBIZART <dwiyohan9@gmail.com>

--- bianca_merlinx.mk
+++ bianca_merlinx.mk
@@ -44,10 +44,10 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 TARGET_VENDOR := xiaomi
 TARGET_VENDOR_PRODUCT_NAME := merlinx
-PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="redfin-user 12 SP1A.211105.002 7961164 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="raven-user 12 SQ1D.220205.003 8069835 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := google/redfin/redfin:12/SP1A.211105.002/7961164:user/release-keys
+BUILD_FINGERPRINT := google/raven/raven:12/SQ1D.220205.003/8069835:user/release-keys
 
 # Gapps
 TARGET_BOOT_ANIMATION_RES := 1080
